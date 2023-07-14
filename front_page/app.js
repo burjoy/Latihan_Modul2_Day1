3f68a7f15b585d861160517ffc16fa94d990df89
Luqman <51292479+burjoy@users.noreply.github.com>
2023-07-14 19:03:04 +0700

    Update app.js

--- front_page/app.js
+++ front_page/app.js
@@ -70,6 +70,18 @@ document.addEventListener("DOMContentLoaded", () => {
         audio.classList.add('w-full');
         audio.controls = true;
         musicCard.appendChild(audio);
+
+        const buttonContainer = document.createElement('div');
+        buttonContainer.classList.add("flex", "justify-between", "mt-4");
+        const button_update = document.createElement('button');
+        const button_remove = document.createElement('button');
+        button_update.classList.add("bg-blue-500", "text-white", "rounded-full", "px-4", "py-2", "mr-2", "updateBtn");
+        button_remove.classList.add("bg-red-500", "text-white", "rounded-full", "px-4", "py-2", "deleteBtn");
+        button_update.innerText = "Update";
+        button_remove.innerText = "Delete";
+        buttonContainer.appendChild(button_update);
+        buttonContainer.appendChild(button_remove);
+        musicCard.appendChild(buttonContainer);
   
         return musicCard;
     }
@@ -84,37 +96,110 @@ document.addEventListener("DOMContentLoaded", () => {
         const songTitle = songTitleInput.value;
         const artistName = artistNameInput.value;
         const songURL = songURLInput.value;
+        let song_id = Math.floor(Math.random()*(999-100+1)+100);
 
-        const data = {
-            title: songTitle,
-            artist: artistName,
-            path: songURL
-        };
+        // fetch('/new_songs', options)
+        // .then((response) => {console.log(response)});
 
-        const options = {
-            method: "POST",
-            headers : {'Content-Type':'application/json'},
-            body: JSON.stringify(data)
-        };
-
-        fetch('/new_songs', options)
-        .then((response) => {console.log(response)});
-  
-        if (songTitle !== '' && artistName !== '' && songURL !== '') {
-          const musicCard = createMusicCard(songTitle, artistName, songURL);
-          musicContainer.appendChild(musicCard);
-  
-          // Reset form inputs
-          songTitleInput.value = '';
-          artistNameInput.value = '';
-          songURLInput.value = '';
-        }
+        postData(songTitle, artistName, songURL, song_id)
+        .then(response => {
+          console.log(response)
+          if (songTitle !== '' && artistName !== '' && songURL !== '') {
+            const musicCard = createMusicCard(songTitle, artistName, songURL);
+            musicContainer.appendChild(musicCard);
+    
+            // Reset form inputs
+            songTitleInput.value = '';
+            artistNameInput.value = '';
+            songURLInput.value = '';
+          }
+        });
       });
+
+      function createUpdateForm(title, artist, url) {
+        const updateForm = document.createElement('form');
+        // updateForm.classList.add('flex');
+  
+        const titleInput = document.createElement('input');
+        titleInput.type = 'text';
+        titleInput.value = title;
+        titleInput.classList.add('mr-2', 'px-4', 'py-2', 'border', 'border-gray-300');
+        updateForm.appendChild(titleInput);
+  
+        const artistInput = document.createElement('input');
+        artistInput.type = 'text';
+        artistInput.value = artist;
+        artistInput.classList.add('mr-2', 'px-4', 'py-2', 'border', 'border-gray-300');
+        updateForm.appendChild(artistInput);
+  
+        const urlInput = document.createElement('input');
+        urlInput.type = 'text';
+        urlInput.value = url;
+        urlInput.classList.add('mr-2', 'px-4', 'py-2', 'border', 'border-gray-300');
+        updateForm.appendChild(urlInput);
+  
+        const updateButton = document.createElement('button');
+        updateButton.type = 'submit';
+        updateButton.textContent = 'Update';
+        updateButton.classList.add('bg-blue-500', 'text-white', 'rounded-full', 'px-4', 'py-2');
+        updateForm.appendChild(updateButton);
+
+        updateForm.addEventListener('submit', (e) => {
+          e.preventDefault();
+    
+          const updatedTitle = titleInput.value.trim();
+          const updatedArtist = artistInput.value.trim();
+          const updatedURL = urlInput.value.trim();
+    
+          if (updatedTitle !== '' && updatedArtist !== '' && updatedURL !== '') {
+            const updatedMusicCard = createMusicCard(updatedTitle, updatedArtist, updatedURL);
+            updateForm.closest('.bg-white').replaceWith(updatedMusicCard);
+          }
+        });
+        return updateForm;
+    }
+
+    musicContainer.addEventListener('click', (e) => {
+      const target = e.target;
+
+      if (target.classList.contains('deleteBtn')) {
+        const musicCard = target.closest('.bg-white');
+        musicCard.remove();
+      } else if (target.classList.contains('updateBtn')) {
+        const musicCard = target.closest('.bg-white');
+        const title = musicCard.querySelector('h2').textContent;
+        const artist = musicCard.querySelector('p').textContent;
+        const audioSrc = musicCard.querySelector('audio').src;
+
+        const updateForm = createUpdateForm(title, artist, audioSrc);
+        musicCard.innerHTML = '';
+        musicCard.appendChild(updateForm);
+      }
+    });
 })
 
 async function ambilData(){
     const songs = await fetch("http://localhost:3000/songs");
     const hasil = await songs.json();
     return hasil;
+}
+
+async function postData(songTitle, artistName, songURL, song_id){
+  const data = {
+    title: songTitle,
+    artist: artistName,
+    path: songURL, 
+    id: song_id
+  };
+
+  const options = {
+    method: "POST",
+    headers : {'Content-Type':'application/json'},
+    body: JSON.stringify(data)
+  };
+
+  const newSong = await fetch('http://localhost:3000/songs', options);
+  const json = newSong.json();
+  return json;
 }
 console.log(ambilData());
